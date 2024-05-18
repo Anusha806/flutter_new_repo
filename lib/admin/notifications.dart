2033b25de928bb5c76cc2440484aebec7e825d7c
Anusha806 <anushamajji806@gmail.com>
2024-05-18 11:25:23 +0530

    3rd commit

---
+++ lib/admin/notifications.dart
@@ -0,0 +1,15 @@
+// TODO Implement this library.
+import 'package:flutter/material.dart';
+
+class Notifications extends StatelessWidget {
+  // const Notifications({required Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Notifications'),
+      ),
+    );
+  }
+}
